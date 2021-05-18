5aeaef56235858600041b717a70f587b02eaf6b1
Jose Daniel <jrave@ucm.es>
2021-05-19 01:18:23 +0200

    Añadido Blur a los niveles

--- AstralChartGame/AstralChartGame/Stages/Lev1.xaml.cs
+++ AstralChartGame/AstralChartGame/Stages/Lev1.xaml.cs
@@ -56,10 +56,19 @@ namespace AstralChartGame.Stages
 
         }
 
-        private void select_level(object sender, RoutedEventArgs e)
-        {
-            if (A == null)
-            {
+        private void change_style(object sender, RoutedEventArgs e){
+            Button n = (Button)sender;
+            Style s = Application.Current.Resources["StarUnavailable"] as Style;
+
+            n.Style = s;
+        }
+
+        private void select_level(object sender, RoutedEventArgs e){
+
+            int stageInLevel = 1;
+            Button n =  Utils.FindControl<Button>(Level, "star" + stageInLevel.ToString());
+
+            if (A == null){
                 A = (Button)sender;
             }
             else if (B == null)
@@ -67,15 +76,14 @@ namespace AstralChartGame.Stages
                 B = (Button)sender;
             }
 
-            if (A != null && B != null)
-            {
+            if (A != null && B != null){
                 // Create a Line  
                 Line redLine = new Line();
 
-                redLine.X1 = Level.Margin.Left + A.Margin.Left - (A.ActualWidth / 1);
-                redLine.Y1 = Level.Margin.Top + A.Margin.Top + (A.ActualHeight / 2);
-                redLine.X2 = Level.Margin.Left + B.Margin.Left - (B.ActualWidth / 1);
-                redLine.Y2 = Level.Margin.Top + B.Margin.Top + (B.ActualHeight / 2);
+                redLine.X1 = Level.Margin.Left + A.Margin.Left - (A.ActualWidth*0.85);
+                redLine.Y1 = Level.Margin.Top + A.Margin.Top + (A.ActualHeight*0.45);
+                redLine.X2 = Level.Margin.Left + B.Margin.Left - (B.ActualWidth * 0.85);
+                redLine.Y2 = Level.Margin.Top + B.Margin.Top + (B.ActualHeight * 0.45);
 
                 // Create a red Brush  
                 SolidColorBrush redBrush = new SolidColorBrush();
@@ -84,6 +92,7 @@ namespace AstralChartGame.Stages
                 // Set Line's width and color  
                 redLine.StrokeThickness = 4;
                 redLine.Stroke = redBrush;
+                redLine.StrokeDashArray = new DoubleCollection() { 2 };
 
                 // Add line to the Grid.  
                 RelativePanel panel = father.Child as RelativePanel;
