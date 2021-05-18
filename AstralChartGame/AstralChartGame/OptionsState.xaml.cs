330ede61ad4b191add2722dca8121e8c9b904b98
Oxelion <juandiem@ucm.es>
2021-05-18 17:29:06 +0200

    Opciones a partirte la cara

--- AstralChartGame/AstralChartGame/OptionsState.xaml.cs
+++ AstralChartGame/AstralChartGame/OptionsState.xaml.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +27,77 @@ namespace AstralChartGame
         public OptionsState()
         {
             this.InitializeComponent();
+            this.NavigationCacheMode =
+            Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+            //Check Options states
+            var view = ApplicationView.GetForCurrentView();
+            if (view.IsFullScreenMode)
+                FullScreen_CheckBox.IsChecked = true;
+            else
+                FullScreen_CheckBox.IsChecked = false;
         }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+        }
+
+        #region goBack
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            On_BackRequested();
+        }
+
+        private bool On_BackRequested()
+        {
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        #region FullScreen
+        private void ToggleFullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            var view = ApplicationView.GetForCurrentView();
+            if (view.IsFullScreenMode)
+            {
+                view.ExitFullScreenMode();
+                ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.Auto;
+                // The SizeChanged event will be raised when the exit from full-screen mode is complete.
+            }
+            else
+            {
+                if (view.TryEnterFullScreenMode())
+                {
+                    ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
+                    // The SizeChanged event will be raised when the entry to full-screen mode is complete.
+                }
+            }
+        }
+        #endregion
+
+        #region Language
+        private void SetAppLanguage(object sender, object e)
+        {
+            int index = (sender as ComboBox).SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    break;
+                case 1:
+                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    break;
+                case 2:
+                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    break;
+            }
+            this.Frame.Navigate(this.GetType());
+        }
+        #endregion
     }
 }
