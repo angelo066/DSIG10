dfc4d698f70437d74a54a48c771013e0edfbc897
Oxelion <juandiem@ucm.es>
2021-05-25 21:15:03 +0200

    OptionsState Casi terminado y cambiado el idioma general del juego a Español

--- AstralChartGame/AstralChartGame/OptionsState.xaml.cs
+++ AstralChartGame/AstralChartGame/OptionsState.xaml.cs
@@ -16,6 +16,9 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Globalization;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Media.Imaging;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,16 +29,20 @@ namespace AstralChartGame
     /// </summary>
     public sealed partial class OptionsState : Page
     {
-        MediaPlayer musicPlayer, sfxPlayer;
+        int lang = 0;
+        MediaPlayer musicPlayer, sfxPlayer; 
+        MediaElement mediaElement;
         double mainVolume;
         double musicVolume;
         double sfxVolume;
+        bool narrator = true;
         public OptionsState()
         {
             this.InitializeComponent();
             //Audio
             musicPlayer = new MediaPlayer();
             sfxPlayer = new MediaPlayer();
+            mediaElement = new MediaElement();
             Init_BGMusic();
 
             this.NavigationCacheMode =
@@ -46,8 +53,26 @@ namespace AstralChartGame
                 FullScreen_CheckBox.IsChecked = true;
             else
                 FullScreen_CheckBox.IsChecked = false;
+
+            if (narrator)
+                NarratorBox.IsChecked = true;
+            else
+                NarratorBox.IsChecked = false;
+
+            LanguageBox.SelectedIndex = lang;
         }
         #region Audio
+        private async void speak(string option)
+        {
+            //mediaElement = new MediaElement();
+            var synth = new Windows.Media.SpeechSynthesis.SpeechSynthesizer();
+
+            Windows.Media.SpeechSynthesis.SpeechSynthesisStream stream =
+                await synth.SynthesizeTextToStreamAsync(option);
+
+            mediaElement.SetSource(stream, stream.ContentType);
+            mediaElement.Play();
+        }
         private async void Init_BGMusic()
         {
             Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync(@"Assets\audios");
@@ -65,18 +90,24 @@ namespace AstralChartGame
                 musicPlayer.Volume = mainVolume * musicVolume;
                 sfxPlayer.Volume = mainVolume * sfxVolume;
             }
+            if (narrator)
+                speak((mainVolume * 100).ToString());
         }
         private void changeMusicVolume(object sender, RangeBaseValueChangedEventArgs e)
         {
             musicVolume = (sender as Slider).Value / 100;
             if (musicPlayer != null)
                 musicPlayer.Volume = mainVolume * musicVolume;
+            if (narrator)
+                speak((musicVolume * 100).ToString());
         }
         private void changeSfxVolume(object sender, RangeBaseValueChangedEventArgs e)
         {
             sfxVolume = (sender as Slider).Value / 100;
             if (sfxPlayer != null)
                 sfxPlayer.Volume = mainVolume * sfxVolume;
+            if (narrator)
+                speak((sfxVolume * 100).ToString());
         }
         private async void soundOverButton(object sender, PointerRoutedEventArgs e)
         {
@@ -87,6 +118,21 @@ namespace AstralChartGame
             sfxPlayer.Source = MediaSource.CreateFromStorageFile(file);
 
             sfxPlayer.Play();
+
+            string tag;
+            if ((sender as Button) != null)
+                tag = (sender as Button).Tag.ToString();
+            else if((sender as CheckBox) != null)
+                tag = (sender as CheckBox).Tag.ToString();
+            else if((sender as Slider) != null)
+                tag = (sender as Slider).Tag.ToString();
+            else if((sender as ComboBoxItem) != null)
+                tag = (sender as ComboBoxItem).Tag.ToString();
+            else
+                tag = (sender as ComboBox).Tag.ToString();
+
+            if (narrator)
+                speak("Estàs sobre" + tag);
         }
 
         private async void soundClickButton(object sender, RoutedEventArgs e)
@@ -98,6 +144,17 @@ namespace AstralChartGame
             sfxPlayer.Source = MediaSource.CreateFromStorageFile(file);
 
             sfxPlayer.Play();
+
+            string tag;
+            if ((sender as Button) != null)
+                tag = (sender as Button).Tag.ToString();
+            else if ((sender as CheckBox) != null)
+                tag = (sender as CheckBox).Tag.ToString();
+            else
+                tag = (sender as Slider).Tag.ToString();
+
+            if (narrator)
+                speak("Pulsaste" + tag);
         }
         private async void soundClickButton(object sender, object e)
         {
@@ -108,6 +165,20 @@ namespace AstralChartGame
             sfxPlayer.Source = MediaSource.CreateFromStorageFile(file);
 
             sfxPlayer.Play();
+            string tag;
+            if ((sender as Button) != null)
+                tag = (sender as Button).Tag.ToString();
+            else if ((sender as CheckBox) != null)
+                tag = (sender as CheckBox).Tag.ToString();
+            else if ((sender as Slider) != null)
+                tag = (sender as Slider).Tag.ToString();
+            else if ((sender as ComboBoxItem) != null)
+                tag = (sender as ComboBoxItem).Tag.ToString();
+            else
+                tag = (sender as ComboBox).Tag.ToString();
+
+            if (narrator)
+                speak("Pulsaste" + tag);
         }
         #endregion
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -142,6 +213,8 @@ namespace AstralChartGame
                 view.ExitFullScreenMode();
                 ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.Auto;
                 // The SizeChanged event will be raised when the exit from full-screen mode is complete.
+                if (narrator)
+                    speak("Modo Ventana");
             }
             else
             {
@@ -149,10 +222,12 @@ namespace AstralChartGame
                 {
                     ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.FullScreen;
                     // The SizeChanged event will be raised when the entry to full-screen mode is complete.
+                    if(narrator)
+                        speak("Modo Pantalla Completa");
                 }
             }
-            soundClickButton(sender, e);
         }
+
 
         #endregion
 
@@ -163,18 +238,56 @@ namespace AstralChartGame
             switch (index)
             {
                 case 0:
-                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    ApplicationLanguages.PrimaryLanguageOverride = "es";
+                    lang = 0;
                     break;
                 case 1:
-                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    ApplicationLanguages.PrimaryLanguageOverride = "en";
+                    lang = 1;
                     break;
                 case 2:
-                    ApplicationLanguages.PrimaryLanguageOverride = "fr";
+                    ApplicationLanguages.PrimaryLanguageOverride = "zh-Hans";
+                    
+                    lang = 2;
                     break;
             }
-            this.Frame.Navigate(this.GetType());
+            Windows.ApplicationModel.Resources.Core.ResourceContext.GetForCurrentView().Reset();
+            Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse().Reset();
+            Frame.Navigate(this.GetType());
+
+            soundClickButton(sender, e);
         }
+
         #endregion
 
+        #region ColorBlindness
+        private void SetColorMode(object sender, object e)
+        {
+            int index = (sender as ComboBox).SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    BG_Image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Stars.jpg"));
+                    break;
+                case 1:
+                    BG_Image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Stars_Prot.png"));
+                    break;
+                case 2:
+                    BG_Image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Stars_Deu.png"));
+                    break;
+                case 3:
+                    BG_Image.Source = new BitmapImage(new Uri("ms-appx:///Assets/Stars_Tri.png"));
+                    break;
+            }
+
+            soundClickButton(sender, e);
+        }
+        #endregion
+        private void narratorClick(object sender, RoutedEventArgs e)
+        {
+            narrator = !narrator;
+            if (narrator)
+                speak("Narrador Activado");
+        }
     }
 }
